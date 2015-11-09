78ed7eb76d1c749d57c838ebe274157a01d94200
WslF <WasylF@gmail.com>
2015-11-09 20:15:53 +0200

    add clear StrategyWslf and StrategyBuggy1x4

---
+++ CodeRacing/src/main/java/StrategyBuggy1x4.java
@@ -0,0 +1,15 @@
+package main.java;
+
+import model.Car;
+import model.Game;
+import model.Move;
+import model.World;
+
+/**
+ *
+ * @author Wsl_F
+ */
+public class StrategyBuggy1x4 extends StrategyWslF{
+            public void move(Car self, World world, Game game, Move move) {
+        }
+}
