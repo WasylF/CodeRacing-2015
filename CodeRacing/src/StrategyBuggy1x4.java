39277e2b5601e5799035dc253bc12a0a3ea51875
WslF <WasylF@gmail.com>
2015-11-11 00:26:58 +0200

    add method distance to wall

--- CodeRacing/src/StrategyBuggy1x4.java
+++ CodeRacing/src/StrategyBuggy1x4.java
@@ -1,6 +1,8 @@
 
 import java.lang.*;
 import static java.lang.StrictMath.*;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
 import model.Car;
 import model.Game;
 import model.Move;
@@ -13,8 +15,10 @@ import model.World;
 public class StrategyBuggy1x4 extends StrategyWslF {
 
     public void move(Car self, World world, Game game, Move move) {
-//        super.move(self, world, game, move);
         initAll(self, world, game, move);
+
+        int[][] curTile = getCurTile();
+
         double nextWaypointX = (self.getNextWaypointX() + 0.5D) * game.getTrackTileSize();
         double nextWaypointY = (self.getNextWaypointY() + 0.5D) * game.getTrackTileSize();
 
@@ -45,13 +49,75 @@ public class StrategyBuggy1x4 extends StrategyWslF {
 
         move.setWheelTurn(angleToWaypoint * 32.0D / PI);
         move.setEnginePower(0.75D);
-        
-        int[][] curTile= getCurTile();
-        
+
         if (speedModule * speedModule * abs(angleToWaypoint) > 2.5D * 2.5D * PI) {
             move.setBrake(true);
         }
 
+    }
+
+    int getDistanceToWall(int[][] curTile) {
+        Point carSpeed = new Point(self.getSpeedX(), self.getSpeedY());
+        carSpeed.normalize();
+        //double angle = carSpeed.getAngleToOX();
+        {
+            int x = selfX;
+            int y = selfY;
+            double k = 1;
+            try {
+                if (carSpeed.x > 1e-3) {
+                    k = 1 / carSpeed.x;
+                } else {
+                    k = 1 / carSpeed.y;
+                }
+            } catch (Exception ex) {
+                k = 1;
+            }
+
+            int count = 0;
+            while (x < tileSize && y < tileSize && x >= 0 && y >= 0
+                    && (curTile[x][y] == selfCar
+                    || (count < self.getHeight() / 4 && curTile[x][y] == empty))) {
+                if (count > 0) {
+                    count++;
+                }
+                x += (int) k * carSpeed.x + 0.1;
+                y += (int) k * carSpeed.y + 0.1;
+                if (x < tileSize && y < tileSize && x >= 0 && y >= 0
+                        && curTile[x][y] == empty) {
+                    count = 1;
+                }
+            }
+
+            if (x == tileSize || y == tileSize || x <= 0 || y <= 0) {
+                return tileSize;
+            }
+
+            curTile[x][y] = 1;
+            Queue<Integer> q = new LinkedBlockingQueue<>();
+            q.add(x * tileSize + y);
+            while (!q.isEmpty()) {
+                x = q.peek() / tileSize;
+                y = q.peek() % tileSize;
+                q.remove();
+                for (int i = -1; i <= 1; i++) {
+                    for (int j = -1; j <= 1; j++) {
+                        if (abs(i + j) != 2) {
+                            if (x + i >= 0 && x + i < tileSize && y + j >= 0 && y + i < tileSize) {
+                                if (curTile[x + i][y + j] == wall) {
+                                    return curTile[x][y];
+                                } 
+                                if (curTile[x+i][y+j] == empty) {
+                                    curTile[x+i][y+j]= curTile[x][y]+1;
+                                    q.add((x+i)*tileSize+y+j);
+                                }
+                              
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     int[][] getCurTile() {
@@ -106,8 +172,8 @@ public class StrategyBuggy1x4 extends StrategyWslF {
 
     private Point[] getCarVertexCoordinates() {
         Point carCenter = new Point(self.getX() - curTileX * game.getTrackTileSize(), self.getY() - curTileY * game.getTrackTileSize());
-        int carX = (int) (carCenter.x + 0.1);
-        int carY = (int) (carCenter.y + 0.1);
+        //int carX = (int) (carCenter.x + 0.1);
+        //int carY = (int) (carCenter.y + 0.1);
 
         double angle = -self.getAngle();
         int carWidth = (int) self.getWidth();
