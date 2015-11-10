88d582bbf025f1e6183759a6bab5ea88087468ec
WslF <WasylF@gmail.com>
2015-11-10 19:42:25 +0200

    add method checkPointInPolygon

--- CodeRacing/src/Point.java
+++ CodeRacing/src/Point.java
@@ -56,11 +56,42 @@ public class Point extends Object {
     public double multiplyVectors(Point p2) {
         return x * p2.y - y * p2.x;
     }
-    
+
     public int sgnMultiplyVectors(Point p2) {
-        double t= multiplyVectors(p2);
-        if (Math.abs(t)<1e-6) return 0;
-        if (t>0) return 1;
+        double t = multiplyVectors(p2);
+        if (Math.abs(t) < 1e-6) {
+            return 0;
+        }
+        if (t > 0) {
+            return 1;
+        }
         return -1;
     }
+
+    public boolean checkPointInPolygon(Point[] polygon) {
+        int plus;
+        plus = 0;
+        for (int i = 0; i + 1 < polygon.length; i++) {
+            Point v = new Point(polygon[i + 1].x - polygon[i].x, polygon[i + 1].y - polygon[i].y);
+            Point vP = new Point(x - polygon[i].x, y - polygon[i].y);
+            switch (v.sgnMultiplyVectors(vP)) {
+                case 0:
+                    return true;
+                case 1:
+                    plus++;
+            }
+        }
+        
+        Point v = new Point(polygon[0].x - polygon[polygon.length - 1].x, polygon[0].y - polygon[polygon.length - 1].y);
+        Point vP = new Point(x - polygon[polygon.length - 1].x, y - polygon[polygon.length - 1].y);
+        switch (v.sgnMultiplyVectors(vP)) {
+            case 0:
+                return true;
+            case 1:
+                plus++;
+        }
+
+        return (plus == 0 || plus == polygon.length);
+    }
+
 }
