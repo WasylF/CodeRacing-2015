5aa8aec26c5a15bd6d9a6aef7d94c6c2d44d3001
Vasyl Franchuk <Vasyl Franchuk>
2015-11-11 13:03:43 +0200

    new realization of get distance to wall

--- CodeRacing/src/StrategyBuggy1x4.java
+++ CodeRacing/src/StrategyBuggy1x4.java
@@ -54,12 +54,10 @@ public class StrategyBuggy1x4 extends StrategyWslF {
             if (distanceToWall >= tileSize / 2 && speedModule > 0) {
                 move.setUseNitro(true);
             }
+        } else if (distanceToWall > self.getHeight()) {
+            move.setEnginePower(0.85D);
         } else {
-            if (distanceToWall > self.getHeight()) {
-                move.setEnginePower(0.85D);
-            } else {
-                move.setEnginePower(0.5D);
-            }
+            move.setEnginePower(0.5D);
         }
 
         if (world.getTick() != 0 && world.getTick() % 555 == 0) {
@@ -85,80 +83,45 @@ public class StrategyBuggy1x4 extends StrategyWslF {
             return tileSize;
         }
         //double angle = carSpeed.getAngleToOX();
-        {
-            int x = selfX;
-            int y = selfY;
-            double k = 1;
-            try {
-                if (abs(carSpeed.x) >= abs(carSpeed.y)) {
-                    k = signum(carSpeed.x) / carSpeed.x;
-                    while (abs(k * carSpeed.x) < 10) {
-                        k *= 2;
-                    }
+        int numberOfTurns = 20;
+        double deltaAngle = PI / 6;
+        double turnAngle = 2 * deltaAngle / numberOfTurns;
 
-                } else {
-                    k = signum(carSpeed.y) / carSpeed.y;
-                    while (abs(k * carSpeed.y) < 10) {
-                        k *= 2;
-                    }
-                }
-            } catch (Exception ex) {
-                k = 1;
-            }
+        for (int d = 1; d <= tileSize; d++) {
+            Point leftVector = new Point(carSpeed);
+            leftVector.rotateVector(-deltaAngle);
 
-            int count = 0;
-            while (x < tileSize && y < tileSize && x >= 0 && y >= 0
-                    && (curTile[x][y] == selfCar
-                    || (count < 10 && curTile[x][y] == empty))) {
-                if (count > 0) {
-                    count++;
-                }
-                x += (int) k * carSpeed.x + 0.1;
-                y += (int) k * carSpeed.y + 0.1;
-                if (x < tileSize && y < tileSize && x >= 0 && y >= 0
-                        /*&& curTile[x][y] != empty && curTile[x][y] != selfCar*/
-                        && curTile[x][y] == wall) {
-                    count = 1;
-                }
-            }
-
-            if (x >= tileSize || y >= tileSize || x <= 0 || y <= 0) {
-                x = min(x, tileSize - 1);
-                x = max(x, 0);
-                y = min(y, tileSize - 1);
-                y = max(y, 0);
-                if (curTile[x][y] == wall) {
-                    return 0;
-                }
-                return tileSize;
-            }
-
-            curTile[x][y] = 1;
-            Queue<Integer> q = new LinkedBlockingQueue<>();
-            q.add(x * tileSize + y);
-            while (!q.isEmpty()) {
-                x = q.peek() / tileSize;
-                y = q.peek() % tileSize;
-                q.remove();
-                for (int i = -1; i <= 1; i++) {
-                    for (int j = -1; j <= 1; j++) {
-                        if (abs(i + j) != 2) {
-                            if (x + i >= 0 && x + i < tileSize && y + j >= 0 && y + i < tileSize) {
-                                if (curTile[x + i][y + j] == wall) {
-                                    return curTile[x][y];
-                                }
-                                if (curTile[x + i][y + j] == empty) {
-                                    curTile[x + i][y + j] = curTile[x][y] + 1;
-                                    q.add((x + i) * tileSize + y + j);
-                                }
-                            }
-                        }
+            numberOfTurns = (int) (deltaAngle * 2 / turnAngle);
+            for (int i = 0; i <= numberOfTurns; i++) {
+                int x = (int) (selfX + i * leftVector.x);
+                int y = (int) (selfY + i * leftVector.y);
+                if (x <= 0 || y <= 0 || x >= tileSize || y >= tileSize) {
+                    x = getAccurateCoordinate(x);
+                    y = getAccurateCoordinate(y);
+                    if (curTile[x][y] == selfCar || curTile[x][y] == empty) {
+                        return tileSize;
+                    } else {
+                        return d;
                     }
                 }
-            }
 
-            return tileSize;
+                leftVector.rotateVector(turnAngle);
+            }
+            deltaAngle -= turnAngle / 10;
         }
+        return tileSize;
+    }
+
+    /**
+     * возвращает корректную координату, то есть от 0 до tileSize-1
+     *
+     * @param t входная координата
+     * @return подкорректированная
+     */
+    int getAccurateCoordinate(double t) {
+        t = max(t, 0);
+        t = min(t, tileSize - 1);
+        return (int) t;
     }
 
     int[][] getCurTile() {
