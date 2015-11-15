4d2b468853ec2d661e98ba7aedad67873b00803b
WslF <WasylF@gmail.com>
2015-11-15 23:28:41 +0200

    add new class - pair of int and int

---
+++ CodeRacing/src/PairIntInt.java
@@ -0,0 +1,21 @@
+
+/**
+ *
+ * @author Wsl_F
+ */
+public class PairIntInt {
+
+    public int first;
+    public int second;
+
+    public PairIntInt() {
+        this.first = 0;
+        this.second = 0;
+    }
+
+    public PairIntInt(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+
+}
